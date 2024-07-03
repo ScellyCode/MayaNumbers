ff707c62dd05c843830fc31e17cb1f29d2e914e6
Scelly <moritz@club-herrmann.de>
2024-07-03 16:47:21 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        new MayaNumberConverter(5222);
+        new MayaNumberConverter(768932746);
     }
 }
--- src/MayaNumberConverter.java
+++ src/MayaNumberConverter.java
@@ -1,18 +1,21 @@
 public class MayaNumberConverter {
     public MayaNumberConverter(int value) {
-        convertValue(value);
+        if(value < 0) {
+            System.out.println("Can't convert negative number");
+        } else {
+            convertValue(value);
+        }
     }
 
     private void convertValue(int num) {
-        if(num / 20 != 0) {
-            convertValue(num / 20);
-        }
+        if(num / 20 != 0) convertValue(num / 20);
         numberToMaya(num % 20);
     }
 
     private void numberToMaya(int num) {
         String stringBuilder = "|".repeat(Math.max(0, num / 5)) +
                 ".".repeat(Math.max(0, num % 5));
+        if(num == 0) stringBuilder += "0";
         System.out.println(stringBuilder);
     }
 }
